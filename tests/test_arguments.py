ba7b7de2e08032c14368533ff5e45dbb9dca3f6b
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-06-12 15:14:13 +0100

    Refactor crontab generation.
    This also adds the ability to generate the crontab from the command
    line.

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -1,35 +1,6 @@
-from StringIO import StringIO
-from contextlib import contextmanager
-import json
-import os
-from tempfile import mkstemp
 from nose.tools import *
-import sys
+from tools import temp_file, json_file, capture_stderr
 from backdrop.collector.arguments import parse_args
-
-@contextmanager
-def temp_file(contents):
-    fd, filename = mkstemp()
-    os.write(fd, contents)
-    yield filename
-    os.close(fd)
-    os.remove(filename)
-
-
-@contextmanager
-def json_file(contents):
-    with temp_file(json.dumps(contents)) as filename:
-        yield filename
-
-
-@contextmanager
-def capture_stderr():
-    old_stderr = sys.stderr
-    new_stderr = StringIO()
-    sys.stderr = new_stderr
-    yield new_stderr
-    sys.stderr = old_stderr
-    new_stderr.close()
 
 
 class TestParseArgs(object):
--- tests/test_crontab.py
+++ tests/test_crontab.py
@@ -1,62 +1,98 @@
-import unittest
+import subprocess
+from nose.tools import *
 from hamcrest import *
+import sys
 
 from tests.tools import temp_file
 from backdrop.collector import crontab
 
 
-class TestManageCrontab(unittest.TestCase):
+class TestGenerateCrontab(object):
     def test_other_cronjobs_are_preserved(self):
         with temp_file("") as jobs_path:
             generated_jobs = crontab.generate_crontab(
-                "app-name",
                 ['other cronjob'],
                 jobs_path,
-                "", "")
+                "/path/to/app")
             assert_that(generated_jobs, has_item("other cronjob"))
 
     def test_some_cronjobs_are_added(self):
         with temp_file("schedule,query.json,config.json") as jobs_path:
             generated_jobs = crontab.generate_crontab(
-                "app-name",
                 [],
                 jobs_path,
-                "app", "python")
+                "/path/to/app")
             assert_that(generated_jobs,
                         has_item(contains_string("schedule")))
             assert_that(generated_jobs,
-                        has_item(contains_string("-q app/query.json")))
+                        has_item(
+                            contains_string("-q /path/to/app/query.json")))
             assert_that(generated_jobs,
-                        has_item(contains_string("-c app/config.json")))
+                        has_item(
+                            contains_string("-c /path/to/app/config.json")))
 
     def test_added_jobs_get_tagged_with_comment(self):
         with temp_file("schedule,query.json,config.json") as jobs_path:
             generated_jobs = crontab.generate_crontab(
-                "app-name",
                 [],
                 jobs_path,
-                "app", "python")
+                "/path/to/app")
 
             assert_that(generated_jobs,
                         has_item(
-                            ends_with('# app-name')))
+                            ends_with('# app')))
 
     def test_existing_tagged_cronjobs_are_purged(self):
         with temp_file("schedule,query.json,config.json") as jobs_path:
             generated_jobs = crontab.generate_crontab(
-                "app-name",
                 ['other job # app-name'],
                 jobs_path,
-                "app", "python"
-            )
+                "/path/to/app")
             assert_that(generated_jobs,
                         is_not(has_item(contains_string("other job"))))
 
     def test_invalid_jobs_file_causes_failure(self):
         with temp_file("afeg    ;efhv   2[\-r u1\-ry1r") as jobs_path:
-            self.assertRaises(crontab.ParseError,
-                              crontab.generate_crontab,
-                              "app-name",
-                              [],
-                              jobs_path,
-                              "app", "python")
+            assert_raises(crontab.ParseError,
+                          crontab.generate_crontab,
+                          [],
+                          jobs_path,
+                          "/path/to/app")
+
+
+class TestCrontabScript(object):
+    def run_crontab_script(self, current_crontab, path_to_app, path_to_jobs):
+        with temp_file(current_crontab) as stdin_path:
+            args = [
+                sys.executable,
+                '-m', 'backdrop.collector.crontab',
+                path_to_app, path_to_jobs
+            ]
+            return subprocess.check_output(
+                args,
+                stdin=open(stdin_path),
+                stderr=subprocess.STDOUT)
+
+    def test_happy_path(self):
+        with temp_file('') as path_to_jobs:
+            output = self.run_crontab_script(
+                'current crontab', '/path/to/app', path_to_jobs)
+            assert_that(output.strip(),
+                        is_('current crontab'))
+
+    def test_with_jobs(self):
+        with temp_file('one,two,three') as path_to_jobs:
+            output = self.run_crontab_script(
+                'current crontab', '/path/to/app', path_to_jobs)
+
+            cronjobs = output.split("\n")
+            assert_that(cronjobs,
+                        has_item(ends_with("# app")))
+
+            assert_that(cronjobs,
+                        has_item(contains_string(sys.executable)))
+
+    def test_failure_results_in_non_zero_exit_status(self):
+        assert_raises(subprocess.CalledProcessError,
+                      self.run_crontab_script,
+                      'current crontab', '/path/to/app', 'invalid path')
