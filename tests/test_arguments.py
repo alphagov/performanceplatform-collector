09ebdfa69a205dfffa9869127bfa8fd8285b9351
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-10-04 14:28:33 +0100

    Add arguments for filtering by start and end dates

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -16,6 +16,35 @@ class TestParseArgs(object):
             assert_raises(
                 SystemExit, parse_args, args=["-c", credentials_path])
 
+    def test_credentials_path_is_required(self):
+        with json_file({}) as query_path:
+            assert_raises(
+                SystemExit, parse_args, args=["-q", query_path])
+
+    def test_start_and_end_fields_are_allowed(self):
+        with json_file({}) as credentials_path:
+            with json_file({}) as query_path:
+                parse_args(
+                    args=[
+                        "-c", credentials_path, "-q", query_path,
+                        "-s", "2013-10-10", "-e", "2013-10-10"])
+
+    def test_start_at_must_be_parsable_as_a_date(self):
+        with json_file({}) as credentials_path:
+            with json_file({}) as query_path:
+                assert_raises(
+                    SystemExit, parse_args, args=[
+                        "-c", credentials_path, "-q", query_path,
+                        "-s", "not-a-date"])
+
+    def test_end_at_must_be_parsable_as_a_date(self):
+        with json_file({}) as credentials_path:
+            with json_file({}) as query_path:
+                assert_raises(
+                    SystemExit, parse_args, args=[
+                        "-c", credentials_path, "-q", query_path,
+                        "-e", "not-a-date"])
+
     def test_query_file_must_be_json(self):
         with capture_stderr() as stderr:
             with temp_file("not json") as query_path:
