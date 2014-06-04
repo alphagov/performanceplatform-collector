724f3200e216c1f3ae4abe4ee3e08fd73905593a
Jacob Ashdown <jcbashdown@gmail.com>
2014-06-04 16:02:00 +0100

    wip, something like this for passed in repeat value/period value

--- performanceplatform/collector/ga/datetimeutil.py
+++ performanceplatform/collector/ga/datetimeutil.py
@@ -25,21 +25,44 @@ def to_utc(a_datetime):
     return a_datetime.astimezone(pytz.UTC)
 
 
-def period_range(start_date, end_date):
-    start_date = to_date(start_date) or a_week_ago()
-    end_date = to_date(end_date) or a_week_ago()
+def period_range(start_date, end_date, repeat):
+    #and start_date/end_date currently never passed in
+    if not repeat:
+        start_date = to_date(start_date) or a_week_ago()
+        end_date = to_date(end_date) or a_week_ago()
 
-    if start_date > end_date:
-        raise ValueError("Bad period: !(start_date={0} <= end_date={1})"
-                         .format(start_date, end_date))
+        if start_date > end_date:
+            raise ValueError("Bad period: !(start_date={0} <= end_date={1})"
+                             .format(start_date, end_date))
 
-    if start_date.weekday != MONDAY:
-        start_date = start_date - timedelta(days=start_date.weekday())
+        if start_date.weekday != MONDAY:
+            start_date = start_date - timedelta(days=start_date.weekday())
 
-    period = timedelta(days=7)
-    while start_date <= end_date:
-        yield (start_date, start_date + timedelta(days=6))
-        start_date += period
+        period = timedelta(days=7)
+        while start_date <= end_date:
+            yield (start_date, start_date + timedelta(days=6))
+            start_date += period
+    if repeat == 'day'
+        #two because with weekly we collect last week again every day
+        #with this we will collect last two days every day
+        #two days ago should return start of day
+        #passed in dates must be verified 2 days apart otherwise generate them
+        start_date = to_date(start_date) or two_days_ago()
+        end_date = to_date(end_date) or two_days_ago()
+
+        if start_date > end_date:
+            raise ValueError("Bad period: !(start_date={0} <= end_date={1})"
+                             .format(start_date, end_date))
+
+        start_date.to_start_of_day()
+
+        #don't go over sensible end date
+        period = timedelta(days=2)
+        while start_date <= end_date:
+            yield (start_date, start_date + timedelta(days=1))
+            start_date += period
+    if repeat == 'hour'
+        #etc. do some stuff
 
 
 def a_week_ago():
