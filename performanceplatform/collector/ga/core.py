17de1d866df4029392831e1bd4bf8a590a1ca755
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-06-16 16:04:43 +0100

    Allow field to be mapped to multiple targets

--- performanceplatform/collector/ga/core.py
+++ performanceplatform/collector/ga/core.py
@@ -86,7 +86,27 @@ def data_id(data_type, timestamp, period, dimension_values):
 
 
 def map_one_to_one_fields(mapping, pairs):
-    return dict((mapping.get(key, key), value) for key, value in pairs.items())
+    """
+    >>> mapping = {'a': 'b'}
+    >>> pairs = {'a': 1}
+    >>> map_one_to_one_fields(mapping, pairs)
+    {'b': 1}
+    >>> mapping = {'a': ['b', 'a']}
+    >>> map_one_to_one_fields(mapping, pairs)
+    {'a': 1, 'b': 1}
+    """
+    mapped_pairs = dict()
+    for key, value in pairs.items():
+        if key in mapping:
+            targets = mapping[key]
+            if not isinstance(targets, list):
+                targets = list(targets)
+            for target in targets:
+                mapped_pairs[target] = value
+        else:
+            mapped_pairs[key] = value
+
+    return mapped_pairs
 
 
 def map_multi_value_fields(mapping, pairs):
@@ -111,6 +131,7 @@ def map_multi_value_fields(mapping, pairs):
 
 
 def apply_key_mapping(mapping, pairs):
+    logging.warn("{} -- {}".format(mapping, pairs))
     return dict(map_one_to_one_fields(mapping, pairs).items() +
                 map_multi_value_fields(mapping, pairs).items())
 
