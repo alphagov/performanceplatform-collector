e4d746ba6c5b842529c9dafb31a90bdd31fee687
Tim Mower <timothy.mower@gmail.com>
2014-06-25 16:17:47 +0100

    Fix namespacing for PyPi installs
    See https://github.com/alphagov/performanceplatform-client/pull/5

--- performanceplatform/__init__.py
+++ performanceplatform/__init__.py
@@ -1,7 +1,1 @@
-# Namespace package: https://docs.python.org/2/library/pkgutil.html
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
