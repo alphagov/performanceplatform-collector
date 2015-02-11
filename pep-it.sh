7f080614469a8c8ab3c1ff6d68abc09ea070022d
Lee Longmore <lee.longmore@gmail.com>
2015-02-11 11:34:22 +0000

    Bump pep8 and ignore 402 error

--- pep-it.sh
+++ pep-it.sh
@@ -3,5 +3,5 @@
 set -e
 
 basedir=$(dirname $0)
-pep8 --ignore=E201,E202,E241,E251 --exclude=tests,config,build,src,venv,*.egg "$basedir"
-pep8 --ignore=E201,E202,E241,E251,E501 "$basedir/tests"
+pep8 --ignore=E201,E202,E241,E251,E402 --exclude=tests,config,build,src,venv,*.egg "$basedir"
+pep8 --ignore=E201,E202,E241,E251,E402,E501 "$basedir/tests"
